5f870aa9091c7f5770dd925b6829588bc2cb86e1
Ionut <ionutputinicabz@gmail.com>
2018-12-15 13:55:58 +0100

    Added Operations file

---
+++ Operations.py
@@ -0,0 +1,27 @@
+x_pos=5
+print(x_pos)
+speed=2.5
+print(speed)
+x_pos=speed #x_pos=2.5
+print(x_pos)
+new_x_pos=x_pos+speed #new_x_pos=5
+print(new_x_pos)
+character_name='Ionut'
+print(character_name)
+full_name=character_name+ " Putinica"
+print(full_name)
+x_pos=5
+print(x_pos)
+mod_x_pos=x_pos%2 #mod_x_pos=1
+print(mod_x_pos)
+floor_div_x_pos=x_pos//2 #floor_div_x_pos=2
+print(floor_div_x_pos)
+x_pos_squared=x_pos**2 #x_pos_squared=25
+print(x_pos_squared)
+x_pos+=5 #x_pos=10
+print(x_pos)
+
+
+
+
+
